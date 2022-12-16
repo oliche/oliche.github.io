7a5ef7a37b0cf7eefb3ec27711d9e8bd55772902
olivier <olivier.winter@hotmail.fr>
2022-12-16 22:41:03 +0000

    update local serve command to specify port

--- serve_local.sh
+++ serve_local.sh
@@ -1,1 +1,2 @@
-bundle exec jekyll serve --config _config.yml,_config_dev.yml --destination ./_site
+bundle exec jekyll serve --config _config.yml,_config_dev.yml --destination ./_site --port 4567
+
